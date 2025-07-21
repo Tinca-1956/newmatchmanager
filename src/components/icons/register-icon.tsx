45d5fbeabb46c4a4a8278f106626089d64678166
Stuart Winton <stuart.thomas.winton@gmail.com>
2025-07-21 11:28:12 +0000

    In the MATCHES page, the REGISTYER icon is not fully visible. Change it

--- src/components/icons/register-icon.tsx
+++ src/components/icons/register-icon.tsx
@@ -4,13 +4,15 @@ export const RegisterIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    fill="currentColor"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     {...props}
   >
-    <path d="M19.125 4.875H9.75a.75.75 0 000 1.5h8.625a.75.75 0 01.75.75v8.625a.75.75 0 001.5 0V6.375a1.5 1.5 0 00-1.5-1.5z" />
-    <path d="M16.5 9H6.375a1.5 1.5 0 00-1.5 1.5V19.125a.75.75 0 001.5 0v-8.625a.75.75 0 01.75-.75h8.625a.75.75 0 000-1.5z" />
-    <circle cx="8.625" cy="15.375" r="1.125" />
-    <circle cx="12.375" cy="15.375" r="1.125" />
-    <path d="M14.06 4.94a.75.75 0 00-1.06-1.06l-4.5 4.5a.75.75 0 001.06 1.06l4.5-4.5z" />
+    <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
+    <polyline points="10 17 15 12 10 7" />
+    <line x1="15" y1="12" x2="3" y2="12" />
   </svg>
 );
