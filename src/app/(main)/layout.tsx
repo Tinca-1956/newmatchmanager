9cdb4d2d25df5b390e1413f175641ab62517744f
Stuart Winton <stuart.thomas.winton@gmail.com>
2025-07-25 00:36:45 +0000

    Make the footer black background with white text

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import AppHeader from '@/components/app-header';
@@ -38,7 +39,7 @@ export default function MainLayout({
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/40">
           {children}
         </main>
-        <footer className="text-center p-4 text-sm text-muted-foreground bg-background border-t">
+        <footer className="text-center p-4 text-sm text-sidebar-foreground bg-sidebar border-t border-sidebar-border">
           Copyright EMANCIUM 2025 - All rights reserved
         </footer>
       </div>
