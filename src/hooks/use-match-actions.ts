524277039bab9c206ebf833a893f9a3026d87de8
Stuart Winton <stuart.thomas.winton@gmail.com>
2025-07-26 19:32:11 +0000

    Runtime error: PAgeNOtFoundError please investigate console

--- src/hooks/use-match-actions.ts
+++ src/hooks/use-match-actions.ts
@@ -28,7 +28,7 @@ export const useMatchActions = () => {
 
   const handleViewResults = (match: Match) => {
     if (isMobile) {
-      router.push(`/main/matches/${match.id}/results`);
+      router.push(`/main/results/${match.id}`);
     } else {
       setSelectedMatchForModal(match);
       setIsResultsModalOpen(true);
