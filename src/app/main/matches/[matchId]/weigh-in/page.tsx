5d53623ea6342e1485827298cd5ef10128dfd320
Stuart Winton <stuart.thomas.winton@gmail.com>
2025-07-27 07:34:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/main/matches/[matchId]/weigh-in/page.tsx
+++ src/app/main/matches/[matchId]/weigh-in/page.tsx
@@ -384,17 +384,34 @@ export default function WeighInPage() {
 
   const renderListView = () => {
     if (isLoading) {
-      return Array.from({length: 5}).map((_, i) => (
-        <TableRow key={i}>
-          <TableCell><Skeleton className="h-4 w-32" /></TableCell>
-          <TableCell><Skeleton className="h-10 w-20" /></TableCell>
-          <TableCell><Skeleton className="h-10 w-20" /></TableCell>
-          <TableCell><Skeleton className="h-10 w-24" /></TableCell>
-          <TableCell><Skeleton className="h-10 w-32" /></TableCell>
-          <TableCell><Skeleton className="h-4 w-12" /></TableCell>
-          <TableCell><Skeleton className="h-10 w-20" /></TableCell>
-        </TableRow>
-      ))
+      return (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Angler</TableHead>
+              <TableHead className="w-[120px]">Peg</TableHead>
+              <TableHead className="w-[120px]">Section</TableHead>
+              <TableHead className="w-[150px]">Weight (Kg)</TableHead>
+              <TableHead className="w-[180px]">Status</TableHead>
+              <TableHead>Rank</TableHead>
+              <TableHead className="w-[100px]">Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 5 }).map((_, i) => (
+              <TableRow key={i}>
+                <TableCell><Skeleton className="h-4 w-32" /></TableCell>
+                <TableCell><Skeleton className="h-10 w-20" /></TableCell>
+                <TableCell><Skeleton className="h-10 w-20" /></TableCell>
+                <TableCell><Skeleton className="h-10 w-24" /></TableCell>
+                <TableCell><Skeleton className="h-10 w-32" /></TableCell>
+                <TableCell><Skeleton className="h-4 w-12" /></TableCell>
+                <TableCell><Skeleton className="h-10 w-20" /></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      );
     }
 
     return (
