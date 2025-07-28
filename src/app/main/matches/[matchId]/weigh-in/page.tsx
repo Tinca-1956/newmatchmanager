95c5647af68073336ecc52b7a0cb0eb59c4f2cae
Stuart Winton <stuart.thomas.winton@gmail.com>
2025-07-28 00:16:01 +0000

    In the mobile version of the WEIGH-IN page (list view) the PEG NO column

--- src/app/main/matches/[matchId]/weigh-in/page.tsx
+++ src/app/main/matches/[matchId]/weigh-in/page.tsx
@@ -388,25 +388,25 @@ export default function WeighInPage() {
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead>Angler</TableHead>
-              <TableHead>Peg No.</TableHead>
-              <TableHead>Section</TableHead>
-              <TableHead>Weight (Kg)</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Rank</TableHead>
-              <TableHead>Action</TableHead>
+              <TableHead className="w-[120px]">Angler</TableHead>
+              <TableHead className="w-[80px]">Peg No.</TableHead>
+              <TableHead className="w-[80px]">Section</TableHead>
+              <TableHead className="w-[100px]">Weight (Kg)</TableHead>
+              <TableHead className="w-[120px]">Status</TableHead>
+              <TableHead className="w-[80px]">Rank</TableHead>
+              <TableHead className="w-[100px]">Action</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {Array.from({ length: 5 }).map((_, i) => (
               <TableRow key={i}>
-                <TableCell><Skeleton className="h-4 w-32" /></TableCell>
-                <TableCell><Skeleton className="h-10 w-20" /></TableCell>
-                <TableCell><Skeleton className="h-10 w-20" /></TableCell>
-                <TableCell><Skeleton className="h-10 w-24" /></TableCell>
-                <TableCell><Skeleton className="h-10 w-32" /></TableCell>
-                <TableCell><Skeleton className="h-4 w-12" /></TableCell>
-                <TableCell><Skeleton className="h-10 w-20" /></TableCell>
+                <TableCell><Skeleton className="h-4 w-full" /></TableCell>
+                <TableCell><Skeleton className="h-10 w-full" /></TableCell>
+                <TableCell><Skeleton className="h-10 w-full" /></TableCell>
+                <TableCell><Skeleton className="h-10 w-full" /></TableCell>
+                <TableCell><Skeleton className="h-10 w-full" /></TableCell>
+                <TableCell><Skeleton className="h-4 w-full" /></TableCell>
+                <TableCell><Skeleton className="h-10 w-full" /></TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -418,12 +418,12 @@ export default function WeighInPage() {
         <Table>
             <TableHeader>
                 <TableRow>
-                    <TableHead>Angler</TableHead>
-                    <TableHead>Peg No.</TableHead>
-                    <TableHead>Section</TableHead>
-                    <TableHead>Weight (Kg)</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Rank</TableHead>
+                    <TableHead className="w-[25%]">Angler</TableHead>
+                    <TableHead className="w-[15%]">Peg No.</TableHead>
+                    <TableHead className="w-[15%]">Section</TableHead>
+                    <TableHead className="w-[15%]">Weight (Kg)</TableHead>
+                    <TableHead className="w-[15%]">Status</TableHead>
+                    <TableHead className="w-[15%]">Rank</TableHead>
                     <TableHead>Action</TableHead>
                 </TableRow>
             </TableHeader>
