621b38171ffad2e903a6f9c335a4c0c616948d6c
Stuart Winton <stuart.thomas.winton@gmail.com>
2025-08-11 05:46:37 +0000

    FirebaseError: Firebase Storage: No default bucket found. Did you set th

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -17,7 +17,7 @@ const firebaseConfig: FirebaseOptions = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_Bucket,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
@@ -35,8 +35,7 @@ if (typeof window !== 'undefined') {
         
         auth = getAuth(app);
         firestore = getFirestore(app);
-        // Correctly initialize storage with the bucket URL
-        storage = getStorage(app, firebaseConfig.storageBucket);
+        storage = getStorage(app);
         const functions = getFunctions(app);
 
         // This is the correct way to conditionally connect to emulators.
