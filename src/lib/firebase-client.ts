8a2d76a194be9eb293d15d32502b355c0cccc4c6
Stuart Winton <stuart.thomas.winton@gmail.com>
2025-08-12 22:27:24 +0000

    REgistration failed

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -35,7 +35,8 @@ if (typeof window !== 'undefined') {
         
         auth = getAuth(app);
         firestore = getFirestore(app);
-        storage = getStorage(app);
+        // Correctly initialize storage with the bucket URL
+        storage = getStorage(app, firebaseConfig.storageBucket);
         const functions = getFunctions(app);
 
         // This is the correct way to conditionally connect to emulators.
