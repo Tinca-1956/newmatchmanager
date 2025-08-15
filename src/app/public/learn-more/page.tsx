176440f9be5b37ae71008c384a249cf702a31e60
Stuart Winton <stuart.thomas.winton@gmail.com>
2025-08-15 05:01:01 +0000

    This is the contents ofthe public/learn-more page: 'use client';
    import

--- src/app/public/learn-more/page.tsx
+++ src/app/public/learn-more/page.tsx
@@ -17,9 +17,6 @@ export default function LearnMorePage() {
                 <Button asChild size="lg">
                     <Link href="mailto:stuart@emancium.com.au?subject=MATCH MANAGER - ADD NEW CLUB ENQUIRY&body=Dear Stuart,%0A%0AI am interested in learning more about MATCH MANAGER for my club.%0A%0AHere are my club details:%0A%0AClub/association name%09%09:%09FILL IN DETAILS HERE%0ANumber of members%09%09%09:%09FILL IN MEMBER NUMBER HERE%0ANumber of matches per year%09:%09FILL IN MATCH NUMBER HERE%0ACountry%09%09%09%09%09:%09FILL IN COUNTRY HERE%0AState/County/Province%09%09%09:%09FILL IN COUNTY HERE%0A%0AWarmest regards%0A%0AYOUR FULL NAME HERE">Get your club added to MATCH MANAGER</Link>
                 </Button>
-                <Button asChild size="lg" variant="secondary">
-                    <Link href="/main/subscriptions">Renew my club subscription</Link>
-                </Button>
             </div>
         </div>
     </div>
