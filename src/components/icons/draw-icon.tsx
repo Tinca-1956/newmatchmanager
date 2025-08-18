9add24b41cc1c4c7f4bb3dc71efa4ea323cbb41d
Stuart Winton <stuart.thomas.winton@gmail.com>
2025-08-18 22:45:17 +0000

    Chnage the DRAW PEGS icon to this:

--- src/components/icons/draw-icon.tsx
+++ src/components/icons/draw-icon.tsx
@@ -1,4 +1,3 @@
-
 import * as React from 'react';
 
 export const DrawIcon = (props: React.SVGProps<SVGSVGElement>) => (
@@ -12,10 +11,8 @@ export const DrawIcon = (props: React.SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M14.5 16.5a5 5 0 0 1-5 0" />
-    <path d="M14 9h3a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-3" />
-    <path d="M14 9V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8" />
-    <path d="m18 16 4-4" />
-    <path d="m18 12 4 4" />
+    <circle cx="12" cy="12" r="10" />
+    <path d="M14.5 9.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+    <path d="M12 14.5h2.5V9.5H10" />
   </svg>
 );
